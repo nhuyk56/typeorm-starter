cb990803eecc4d4317a18452208cd0ec9aa8f85e
y.nguyen <y.nguyen@9thwonder.com>
2022-06-24 13:57:57 +0700

    update textupdate follow

--- testproxy.ts
+++ testproxy.ts
@@ -1,11 +1,13 @@
+// https://www.youtube.com/watch?v=6l0Ij6S-Z-Q
 // http://httpbin.org/get?answer=42
-// curl https://api.myip.com -x 207.148.123.54:32022
+// curl https://api.myip.com -x http://[2001:19f0:4400:6493:5400:04ff:fe0b:3e77]:32022
 const axios = require('axios');
 const responsePromise = async () => {
  const { data } = await axios.get('https://api.my-ip.io/ip', {
     proxy: {
         protocol: 'http',
-        host: '207.148.123.54',
+        host: '[2001:19f0:4400:6493:5400:04ff:fe0b:3e77]',
+        // host: '2001:19f0:4400:6493:5400:04ff:fe0b:3e77::1',
         port: 32022
     }
   });
