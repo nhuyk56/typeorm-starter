464defa96f748abb68a01dbc5384fcc0326e9527
y.nguyen <y.nguyen@9thwonder.com>
2022-06-29 08:00:39 +0700

    http vs https

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -5,11 +5,11 @@ import getSlug from 'slug'
 import { SocksProxyAgent } from 'socks-proxy-agent';
 
 const proxyOptions = `socks5://45.77.250.86:9050`;
-const httpsAgent = new SocksProxyAgent(proxyOptions);
+const agent = new SocksProxyAgent(proxyOptions);
 
 const trimValue = s => (s || '').trim()
 const getHash = s => s && md5(trimValue(s)).toString()
-const axios = Axios.create({ httpsAgent, timeout: 60000 });
+const axios = Axios.create({ httpAgent: agent, httpsAgent: agent, timeout: 60000 });
 
 const forceFunction = async callback => {
   let times = 100
--- testproxy.ts
+++ testproxy.ts
@@ -3,11 +3,11 @@
 // curl https://api.myip.com -x http://[2001:19f0:4400:6493:5400:04ff:fe0b:3e77]:32022
 import { SocksProxyAgent } from 'socks-proxy-agent';
 const proxyOptions = `socks5://45.77.250.86:9050`;
-const httpsAgent = new SocksProxyAgent(proxyOptions);
+const agent = new SocksProxyAgent(proxyOptions);
 
 const axios = require('axios');
 const responsePromise = async () => {
- const { data } = await axios.get('http://httpbin.org/get?answer=42', { httpsAgent });
+ const { data } = await axios.get('http://httpbin.org/get?answer=42', { httpAgent: agent, httpsAgent: agent });
   console.log(data)
 }
 responsePromise()
