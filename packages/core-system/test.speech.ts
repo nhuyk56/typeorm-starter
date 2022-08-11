f254cded18dc04ea38e131d0ff61b151a2a1b9bd
nhuyk56 <nhuyk56@gmail.com>
2022-08-12 06:11:36 +0700

    test all

--- packages/core-system/test.speech.ts
+++ packages/core-system/test.speech.ts
@@ -2,7 +2,7 @@
 // http://httpbin.org/get?answer=42
 // curl https://api.myip.com -x http://[2001:19f0:4400:6493:5400:04ff:fe0b:3e77]:32022
 import fs from 'fs'
-import { axiosProxy, readDataFN, forceFunction } from './src/utility/index'
+import { axiosProxy, readDataFN, forceFunction, getGroupChapterpath, readDir } from './src/utility/index'
 const getSpeechAuth = async () => {
 const res = {}
  const { data } = await axiosProxy.get('https://www.onenote.com/learningtools')
@@ -17,9 +17,8 @@ const res = {}
   return res
 }
 
-const getContentModelForReader = async ({ headers }) => {
+const getContentModelForReader = async ({ headers, content }) => {
   const api = 'https://learningtools.onenote.com/learningtoolsapi/v2.0/GetContentModelForReader-Canary'
-  const content = readDataFN('C:/Users/YNN/AppData/Local/Temp/__general__/group/1658321277991-49/e5226e3c2405f40392b035ebbdf9a3d1')
   const payload = {
     "data": {
       "title": "Immersive Reader",
@@ -80,44 +79,53 @@ const GetSpeech = async ({ preferredVoice, sentenceModels, headers }) => {
 }
 
 const main = async () => {
-  console.time('speech')
+  const groupFN = '1658321277991-49'
+  const groupChapterpath = getGroupChapterpath({ groupFN })
+  const files = readDir(groupChapterpath)
+
   const speechAuth = await getSpeechAuth()
   console.log('STEP1', speechAuth)
-  const cmfr = await getContentModelForReader({ headers: speechAuth })
-  console.log('STEP2', 'items', cmfr.items?.length)
-  let sentenceModels = [], length = 0
-  let audio = 'data:audio/mpeg;base64,'
-  let all = []
-  for (let i = 0; i < cmfr?.items?.length; i++) {
-    const item = cmfr.items[i]
-    const nextLength = cmfr?.items[i + 1]?.t?.length || 0
-    const isLast = i === cmfr?.items?.length - 1
-    sentenceModels.push(item)
-    length += item?.t?.length || 0
-    if (isLast || length + nextLength > 2400) {
-      all.push(forceFunction(() => GetSpeech(
-        JSON.parse(
-          JSON.stringify({
-            preferredVoice: 'Male',
-            sentenceModels,
-            headers: cmfr.headers
-          })
-        )
-      )))
-      console.table({ STEP3: i, itemsLength: sentenceModels.length, contentLength: length })
-      sentenceModels = []
-      length = 0
-      if (all.length === 5) {
-        console.timeLog('speech')
-        const base64Arr = await Promise.all(all)
-        console.timeLog('speech')
-        audio += base64Arr.join('\n')
-        all = []
+  
+  for (const file of files) {
+    console.time(file)
+    console.log('STEP2', 'file', file)
+    const content = readDataFN(`${groupChapterpath}/${file}`)
+    const cmfr = await getContentModelForReader({ headers: speechAuth, content })
+    console.log('STEP2', 'items', cmfr.items?.length)
+    let sentenceModels = [], length = 0
+    let audio = 'data:audio/mpeg;base64,'
+    let all = []
+    for (let i = 0; i < cmfr?.items?.length; i++) {
+      const item = cmfr.items[i]
+      const nextLength = cmfr?.items[i + 1]?.t?.length || 0
+      const isLast = i === cmfr?.items?.length - 1
+      sentenceModels.push(item)
+      length += item?.t?.length || 0
+      if (isLast || length + nextLength > 2400) {
+        all.push(forceFunction(() => GetSpeech(
+          JSON.parse(
+            JSON.stringify({
+              preferredVoice: 'Male',
+              sentenceModels,
+              headers: cmfr.headers
+            })
+          )
+        )))
+        console.table({ STEP3: i, itemsLength: sentenceModels.length, contentLength: length })
+        sentenceModels = []
+        length = 0
+        if (all.length === 1) {
+          console.timeLog(file)
+          const base64Arr = await Promise.all(all)
+          console.timeLog(file)
+          audio += base64Arr.join('\n')
+          all = []
+        }
       }
     }
+    fs.writeFileSync(`${file}.base64.txt`, audio)
+    console.timeEnd(file)
   }
-  fs.writeFileSync('audio.base64.txt', audio)
-  console.timeEnd('speech')
 }
 
 main()
