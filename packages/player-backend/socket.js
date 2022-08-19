5dec0f8776686ee9eb592a023401763e842b8ea4
y.nguyen <y.nguyen@9thwonder.com>
2022-08-19 14:01:50 +0700

    multiple

--- packages/player-backend/socket.js
+++ packages/player-backend/socket.js
@@ -13,9 +13,14 @@ let socket = new ws('wss://ws019.qidianaudio.com/?language=vi', [], {
   }
 })
 
-const audioCTN = { items: [] }
+const items = [{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67096523,"chapter_number":"MzQ2NzYyNDUAAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151199,"chapter_number":"MzQ2Nza1NLQEAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151200,"chapter_number":"MzQ2Nza1NDUGAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151201,"chapter_number":"MzQ2Nza1tDQHAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151202,"chapter_number":"MzQ2NzYzMDUAAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151203,"chapter_number":"MzQ2NzYzMLUEAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151204,"chapter_number":"MzQ2NzYzMDMHAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151205,"chapter_number":"MzQ2NzYzMDcGAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151206,"chapter_number":"MzQ2NzYzsDQEAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151207,"chapter_number":"MzQ2NzYzsLQEAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151208,"chapter_number":"MzQ2NzYzNDADAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151209,"chapter_number":"MzQ2NzYzNTQFAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151210,"chapter_number":"MzQ2NzYztTAFAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151211,"chapter_number":"MzQ2NzYzMzEDAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151212,"chapter_number":"MzQ2NzazMDIBAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151213,"chapter_number":"MzQ2NzazMDIFAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151214,"chapter_number":"MzQ2NzazMDIDAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151215,"chapter_number":"MzQ2NzazNLUAAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151216,"chapter_number":"MzQ2NzazNLUEAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151217,"chapter_number":"MzQ2NzazNDMAAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151218,"chapter_number":"MzQ2NzazNDMEAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151219,"chapter_number":"MzQ2NzazNDMCAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151220,"chapter_number":"MzQ2NzazNDMGAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151221,"chapter_number":"MzQ2NzY3sDADAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151222,"chapter_number":"MzQ2NzY3sDAHAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151223,"chapter_number":"MzQ2NzY3sLAAAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151224,"chapter_number":"MzQ2NzY3sLAEAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151225,"chapter_number":"MzQ2NzY3sDQAAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151226,"chapter_number":"MzQ2NzY3sDQEAA","language":"vi"},{"command":"add_job_listen","voice":0,"novel_slug":"ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi","id":67151227,"chapter_number":"MzQ2NzY3NLEAAA","language":"vi"}]
+const passed = {}
+
+const audioCTN = { items: [], status: -1 }
+
 console.time('startTime')
 socket.on('message', message => {
+  const index = Object.keys(passed).length
   const item = JSON.parse(message)
   if(item.command === 'wait_for_audio' && item.data) {
     audioCTN.items = Array.from({ length: item.data }, () => {})
@@ -23,28 +28,34 @@ socket.on('message', message => {
     audioCTN.cLength = 0
   }
   if(item.command === 'sent_audio') {
-    console.log(`[status] ${++audioCTN.cLength}/${audioCTN.fLength}`, `${item.command}: ${item.status}`)
+    console.log(`[${index}/${items.length}][status] ${++audioCTN.cLength}/${audioCTN.fLength}`, `${item.command}: ${item.status}`)
     audioCTN.items[item.index] = item.data
     if (audioCTN.fLength === audioCTN.cLength) {
       const audioContent = 'data:audio/mpeg;base64,' + audioCTN.items.join('')
-      fs.writeFileSync('./mp3.txt', audioContent, {encoding: "utf8"})
+      const fileName = `./mp3.${index}.txt`
+      fs.writeFileSync(fileName, audioContent, { encoding: "utf8" })
+      audioCTN.status = 0
     }
-  } else console.log(`${item.command}: ${item.status}`)
-
-  if (message.includes('authen_success')) {
-    socket.send(JSON.stringify({
-      "command": "add_job_listen",
-      "voice": 0,
-      "novel_slug": "ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi",
-      "id": 67096523,
-      "chapter_number": "MzQ2NzYyNDUAAA",
-      "language": "vi"
-    }))
+  } else console.log(`[${index}/${items.length}]${item.command}: ${item.status}`)
+  // if (item.command === 'receive_text') console.log(item.data)
+  if (audioCTN.status === 0) {
+    const candiate = items.find(c => !passed[String(c.id)])
+    if (candiate) {
+      const id = String(candiate.id)
+      passed[id] = candiate
+      socket.send(JSON.stringify(candiate))
+      audioCTN.status = 1
+    }
   }
 })
 
-socket.on('open', () => console.log('connection success'))
+socket.on('open', () => {
+  console.log('connection success')
+  audioCTN.status = 0
+  audioCTN.items = []
+})
 socket.on('close', () => {
+  audioCTN.status = -1
   console.log('connection end')
   console.timeEnd('startTime')
 })
